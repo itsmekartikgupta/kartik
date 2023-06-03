d6f8d2e6bf34e7c8c4a5766fb8d46bf81238db76
itsmekartikgupta <80156877+itsmekartikgupta@users.noreply.github.com>
2023-06-03 01:10:36 +0000

    Initial implementation of the website prototype
     This commit adds the initial prototype of the website, including the basic structure, layout, and content. It includes the HTML markup, CSS styles, and minimal functionality. Further improvements and enhancements will be made in subsequent commits.

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-  return("hello world")
-  
-  
-if __name__=='__main__' :
-  app.run(host='0.0.0.0',debug=True)
+    return render_template('home.html')
 
+@app.route('/download')
+def download_resume():
+    resume_path = 'static/resume.pdf'
+    return send_file(resume_path, as_attachment=True, attachment_filename='resume.pdf')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+  
